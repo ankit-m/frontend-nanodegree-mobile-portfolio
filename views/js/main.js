3f7f5caa99952e8357f467b0cb1aa8ff483bf746
Ankit Muchhala <muchhalaankit@gmail.com>
2016-12-19 01:55:58 +0530

    changes to increase PageSpeed and make page 60fps

--- views/js/main.js
+++ views/js/main.js
@@ -449,10 +449,14 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var containers = document.querySelectorAll(".randomPizzaContainer");
+    var newwidths = [];
+    for (var i = 0; i < containers.length; i++) {
+      var dx = determineDx(containers[i], size);
+      newwidths.push((containers[i].offsetWidth + dx) + 'px');
+    }
+    for (var i = 0; i < containers.length; i++) {
+      containers[i].style.width = newwidths[i];
     }
   }
 
@@ -502,8 +506,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var st = document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((st / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
@@ -524,7 +529,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 20; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
